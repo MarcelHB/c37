3a98580123775bd0433a200cd09a409173cf994b
MarcelHB <marscel@googlemail.com>
2011-01-30 03:48:22 +0100

    Test fertig, der die Logik nach Spec-Anfoderung testet

--- tests/test_logic.c
+++ tests/test_logic.c
@@ -1,6 +1,7 @@
-#include "../src/globals.h"
+﻿#include "../src/globals.h"
 #include "../src/map.h"
 #include "../src/map_loader.h"
+#include "../src/action.h"
 
 #define MAP_PLAYER_POS1_X 		6
 #define MAP_PLAYER_POS1_Y 		3
@@ -9,6 +10,18 @@
 #define MAP_VIS_TILE_Y 			2
 #define MAP_DARK_TILE_X 		7
 #define MAP_DARK_TILE_Y 		2
+#define MAP_ITEM_TILE_X 		7
+#define MAP_ITEM_TILE_Y 		2
+#define MAP_DOOR_TILE_X 		4
+#define MAP_DOOR_TILE_Y 		7
+#define MAP_BTN1_TILE_X 		8
+#define MAP_BTN1_TILE_Y 		3
+#define MAP_BTN2_TILE_X 		8
+#define MAP_BTN2_TILE_Y 		4
+#define MAP_WALL_TILE_X 		3
+#define MAP_WALL_TILE_Y 		7
+#define MAP_WALL_EXTDOOR_X		2
+#define MAP_WALL_EXTDOOR_Y      7
 
 int main() {
 	Map* map = NULL;
@@ -43,6 +56,52 @@ int main() {
 		if(!map->tiles[MAP_DARK_TILE_X * map->x + MAP_DARK_TILE_Y].spotted) {
 			printf("correct!\n");
 		} else { printf("\n-> ERROR! spotted!\n"); };
+	}
+	
+	/* Interaktionen vom Spieler */
+	printf("checking interactions...\n");
+	{
+		Tile* item_tile = &map->tiles[MAP_ITEM_TILE_Y * map->x + MAP_ITEM_TILE_X];
+		Tile* door_tile = &map->tiles[MAP_DOOR_TILE_Y * map->x + MAP_DOOR_TILE_X];
+		printf("... moving player to %dx%d and pick up item\n", MAP_ITEM_TILE_X, MAP_ITEM_TILE_Y);
+		player->x = MAP_ITEM_TILE_X; player->y = MAP_ITEM_TILE_Y;
+		spawn_tile_collision(player, item_tile, map);
+		printf("=> checking inventory ...");
+		if(player->inventory_size == 1) {
+			printf("correct!\n");
+		} else { printf("\n-> ERROR! expected 1 but player has %d!\n", player->inventory_size); }
+		printf("=> checking tile ...");
+		if(item_tile->number_of_items == 0) {
+			printf("correct!\n");
+		} else { printf("\n-> ERROR! expected 0 but tile has %d!\n", item_tile->number_of_items); }
+		printf("... open door at %dx%d,\n", MAP_DOOR_TILE_X, MAP_DOOR_TILE_Y);
+		toggle_tile(door_tile, map);
+		printf("=> checking state (open) ...");
+		if(((DoorProperties*)door_tile->properties)->open == 1) {
+			printf("correct!\n");
+		} else { printf("\n-> ERROR! door still closed!\n"); }
+	}
+	
+	/* Schalterzusammenhänge */
+	printf("checking button actions...\n");
+	{
+		Tile* btn1_tile = &map->tiles[MAP_BTN1_TILE_Y * map->x + MAP_BTN1_TILE_X];
+		Tile* btn2_tile = &map->tiles[MAP_BTN2_TILE_Y * map->x + MAP_BTN2_TILE_X];
+		Tile* wall_tile = &map->tiles[MAP_WALL_TILE_Y * map->x + MAP_WALL_TILE_X];
+		Tile* door_tile = &map->tiles[MAP_WALL_EXTDOOR_Y * map->x + MAP_WALL_EXTDOOR_X];
+		printf("... pushing button at %dx%d,\n", MAP_BTN1_TILE_X, MAP_BTN1_TILE_Y);
+		toggle_tile(btn1_tile, map);
+		printf("=> checking state of wall (disappeared)...");
+		if( ((WallProperties*)wall_tile->properties)->floor ) {
+			printf("correct!\n");
+		} else { printf("\n-> ERROR! wall is still there!\n"); }
+		
+		printf("... pushing button at %dx%d,\n", MAP_BTN2_TILE_X, MAP_BTN2_TILE_Y);
+		toggle_tile(btn2_tile, map);
+		printf("=> checking state of door (opened)...");
+		if( ((DoorProperties*)door_tile->properties)->open ) {
+			printf("correct!\n");
+		} else { printf("\n-> ERROR! door is still closed!\n"); }
 	}
 	
 	flush_map(map);
